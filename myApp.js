e5e464ae1c996e9f39829d9b46c7c3ed9e5a04e3
WeePeD <87631731+WeePeD@users.noreply.github.com>
2022-09-13 21:39:23 +0700

    lession 11

--- myApp.js
+++ myApp.js
@@ -105,7 +105,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
+  Person.remove({name:nameToRemove},function(err,data){
+    if(err) return console.log(err);
+    done (null,data)
+  })
 
 
   done(null /*, data*/);
